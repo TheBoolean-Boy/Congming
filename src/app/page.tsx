e06e938b90cbcac3bcd3e9764e0c6791308b7e20
TheBoolean-Boy <saswatrath02@gmail.com>
2025-09-29 10:49:53 +0530

    Project initialized and user authentication Setup

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+
+export default async function Home() {
+  
+
+  return (
+    <div>
+      <Button className=" m-4">Shadcn</Button>
+    </div>
+  );
+}
